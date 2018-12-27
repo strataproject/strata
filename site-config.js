ba153f9ce2dafbf8bc5544f9a9289c5676bfd481
Stef Lewandowski <stef@stef.io>
2018-12-27 10:57:44 +0000

    init

---
+++ site-config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  siteTitle: `Strata`,
+  siteTitleShort: `Strata`,
+  siteDescription: `Sustainability data platform`,
+  siteUrl: `https://strata.netlify.com`,
+  themeColor: `#000`,
+  backgroundColor: `#fff`,
+  pathPrefix: null,
+  logo: path.resolve(__dirname, 'src/images/icon.png'),
+  social: {
+    twitter: `stef`,
+    fbAppId: `966242223397117`,
+  },
+};
