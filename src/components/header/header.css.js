ce5d799b985e118d23028b1de8b34103edc1f5c8
Stef Lewandowski <stef@stef.io>
2019-08-09 14:14:46 +0100

    header

--- src/components/header/header.css.js
+++ src/components/header/header.css.js
@@ -8,12 +8,17 @@ export const HeaderContainer = styled.header`
   align-items: center;
 
   a {
-    color: #757575;
+    color: #272722;
     transition: color 0.2s ease;
     text-decoration: none;
+    letter-spacing: 1px;
+    font-weight: bold;
 
     &:hover {
-      color: inherit;
+      color: #00aa00;
+    }
+    &.active {
+      color: #00dd00;
     }
   }
 `
