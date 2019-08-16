0d80297e645b180bf1012ec7a55c6d6b9f8a8a23
Stef Lewandowski <stef@stef.io>
2019-08-16 16:55:01 +0100

    remove segment

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,25 +69,25 @@ module.exports = {
         },
       },
     },
-    {
-      resolve: `gatsby-plugin-segment-js`,
-      options: {
-        // your segment write key for your production environment
-        // when process.env.NODE_ENV === 'production'
-        // required; non-empty string
-        prodKey: `qmCYUZRPyefuavTpeLq3a0hDCRON53g1`,
+    // {
+    //   resolve: `gatsby-plugin-segment-js`,
+    //   options: {
+    //     // your segment write key for your production environment
+    //     // when process.env.NODE_ENV === 'production'
+    //     // required; non-empty string
+    //     prodKey: ``,
 
-        // if you have a development env for your segment account, paste that key here
-        // when process.env.NODE_ENV === 'development'
-        // optional; non-empty string
-        //devKey: `SEGMENT_DEV_WRITE_KEY`,
+    //     // if you have a development env for your segment account, paste that key here
+    //     // when process.env.NODE_ENV === 'development'
+    //     // optional; non-empty string
+    //     //devKey: `SEGMENT_DEV_WRITE_KEY`,
 
-        // whether you want to include analytics.page()
-        // optional; boolean that defaults to true
-        // if false, then don't forget to manually add it to your codebase manually!
-        trackPage: true,
-      },
-    },
+    //     // whether you want to include analytics.page()
+    //     // optional; boolean that defaults to true
+    //     // if false, then don't forget to manually add it to your codebase manually!
+    //     trackPage: true,
+    //   },
+    // },
     {
     resolve: `gatsby-plugin-favicon`,
       options: {
