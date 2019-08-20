1288efb83e948d3ef6da61a9af67235efda422a3
Stef Lewandowski <stef@stef.io>
2019-08-20 10:00:27 +0100

    use component to havce unique id

--- src/components/patternBox/patternBox.js
+++ src/components/patternBox/patternBox.js
@@ -6,7 +6,7 @@ const Dots = ({id = 'default', style = {}, fill = '#545454'}) => {
   return (
     <svg style={style}>
       <defs>
-        <pattern id={tictactoe-${id}} patternUnits="userSpaceOnUse" width="10" height="10">
+        <pattern id={`tictactoe-${id}`} patternUnits="userSpaceOnUse" width="10" height="10">
           <circle cx="1" cy="1" r="1" fill={fill} />
           <rect width='10' height='10' fill='#fff0' />
         </pattern>
@@ -20,7 +20,7 @@ const DiagonalLines = ({id = 'default', style = {}, fill = '#545454'}) => {
   return (
     <svg style={style}>
       <defs>
-        <pattern patternTransform="rotate(-45)" id=`diagonal-${id}` patternUnits="userSpaceOnUse" width="10" height="10">
+        <pattern patternTransform="rotate(-45)" id={`diagonal-${id}`} patternUnits="userSpaceOnUse" width="10" height="10">
           <line x1='0' y1='0' x2='16' y2='0' strokeLinecap="square" stroke={fill} strokeWidth="2" />
           <rect width='10' height='10' fill='#fff0' />
         </pattern>
@@ -30,24 +30,30 @@ const DiagonalLines = ({id = 'default', style = {}, fill = '#545454'}) => {
   )
 }
 
-const PatternBox = ({ style = {}, fill = '#545454', name = 'dots' }) => {
-  uniqueId.enableUniqueIds(this)
-  const id = this.nextUniqueId()
-  let PatternElement = null
-  switch(name) {
-    case 'dots':
-      PatternElement = Dots
-      break;
-    case 'diagonal':
-      PatternElement = DiagonalLines
-      break;
-    default:
-      PatternElement = Dots
-      break;
+class PatternBox extends React.Component {
+  constructor () {
+    super()
+    uniqueId.enableUniqueIds(this)
   }
-  return (
-    <PatternElement style={style} fill={fill} id={id} />
-  )
+  render () {
+    const { style, fill, name } = this.props
+    const id = this.nextUniqueId()
+    let PatternElement = null
+    switch(name) {
+      case 'dots':
+        PatternElement = Dots
+        break;
+      case 'diagonal':
+        PatternElement = DiagonalLines
+        break;
+      default:
+        PatternElement = Dots
+        break;
+    }
+    return (
+      <PatternElement style={style} fill={fill} id={id} />
+    )
+  }
 }
 
 PatternBox.propTypes = {
