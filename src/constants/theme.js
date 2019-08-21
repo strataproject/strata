a31c0f19c08c35ce8722de3556d2843e5965d661
Stef Lewandowski <stef@stef.io>
2019-08-21 16:00:53 +0100

    blue and green

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,8 +1,8 @@
 export const accent = '#138975'
 export const pink = '#e682a3'
-export const blue = '#14a3c7'
+export const blue = '#159EC1' // was '#14a3c7' 
 export const yellow = '#edd796'
-export const green = '#138975'
+export const green = '#148573' // was '#138975' 
 export const black = '#11464b'
 export const orange = '#d66e60'
 
