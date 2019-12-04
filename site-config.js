a14ca533e4ca0ec9b10d2f06f4cf3c6bccb6c77b
Remy Sharp <remy@remysharp.com>
2019-12-04 18:10:29 +0000

    fix: meta desc

--- site-config.js
+++ site-config.js
@@ -1,16 +1,16 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  siteTitle: `Overlay`,
-  siteTitleShort: `Overlay`,
-  siteDescription: `Sustainability data platform`,
-  siteUrl: `https://www.overlay.earth`,
-  themeColor: `#000`,
-  backgroundColor: `#fff`,
+  siteTitle: 'Overlay',
+  siteTitleShort: 'Overlay',
+  siteDescription: 'Sustainable finance data management platform',
+  siteUrl: 'https://www.overlay.earth',
+  themeColor: '#000',
+  backgroundColor: '#fff',
   pathPrefix: null,
   logo: path.resolve(__dirname, 'src/images/icon.png'),
   social: {
-    twitter: `stef`,
-    fbAppId: `966242223397117`,
+    twitter: 'stef',
+    fbAppId: '966242223397117',
   },
-};
+}
